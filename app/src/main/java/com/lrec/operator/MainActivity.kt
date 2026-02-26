4c7d80e84d5a346a4c0ede750a1cca2fdeb071e5
Silva-SA <SILVAAI7001ANG889911@gmail.com>
2026-02-26 09:14:29 +0300

    Integrate ExoPlayer

--- app/src/main/java/com/lrec/operator/MainActivity.kt
+++ app/src/main/java/com/lrec/operator/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.lrec.operator
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 
 class MainActivity : AppCompatActivity() {
+
+    private var player: ExoPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val playerView = findViewById<PlayerView>(R.id.playerView)
+
+        player = ExoPlayer.Builder(this).build()
+        playerView.player = player
+
+        val mediaItem = MediaItem.fromUri(
+            Uri.parse("https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4")
+        )
+
+        player?.setMediaItem(mediaItem)
+        player?.prepare()
+        player?.play()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        player?.release()
+        player = null
     }
 }
